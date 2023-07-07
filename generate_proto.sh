f7e215ef6321c830df492d7a78dd72146509e5d5
Tobias Jungmann <tobias.jungmann@tum.de>
2023-07-07 15:21:38 +0200

    initial commit after splitting

---
+++ generate_proto.sh
@@ -0,0 +1,8 @@
+rm -rf ./proto
+mkdir -p ./proto
+python3 -m grpc_tools.protoc -I. ./matrix.proto --python_out=./proto --grpc_python_out=./proto
+#python3 -m grpc_tools.protoc -I. ./storageServer.proto --python_out=./proto --grpc_python_out=./proto
+#python3 -m grpc_tools.protoc -I proto --python_out=. --grpc_python_out=. proto/some/folder/*.proto
+touch ./proto/__init__.py
+
+# manually add .proto in the import statement i communication_pb2_grpc.py
