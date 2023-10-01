1a0f36a32ab8814d9eae5b65379905a1d527440b
Tobias Jungmann <tobias.jungmann@tum.de>
2023-10-01 10:57:19 +0200

    show external temperature

--- esp_matrix/matrix_withHA_connection/matrix_withHA_connection.ino
+++ esp_matrix/matrix_withHA_connection/matrix_withHA_connection.ino
@@ -7,13 +7,14 @@
 #include <WiFiClientSecure.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-#include <HTTPClient.h>
-//#include <Arduino_JSON.h>
-#include "credentials.h"
 
 #include <MD_MAX72xx.h>
 #include <MD_Parola.h>
 #include <SPI.h>
+#include <HTTPClient.h>
+//#include <Arduino_JSON.h>
+#include "credentials.h"
+
 
 #include <SpotifyArduino.h>
 #include <SpotifyArduinoCert.h>
@@ -54,109 +55,6 @@ String city = "Munich";
 String countryCode = "DE";
 String jsonBuffer;
 
-void printTime(char* pMsg) {
-}
-/*
-void printText(uint8_t modStart, uint8_t modEnd, char* pMsg) {
-  uint8_t state = 0;
-  uint8_t curLen;
-  uint16_t showLen;
-  uint8_t cBuf[8];
-  int16_t col = ((modEnd + 1) * COL_SIZE) - 1;
-
-  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
-  do {
-    switch (state) {
-      case 0:  // Load the next character from the font table
-        // if we reached end of message, reset the message pointer
-        if (*pMsg == '\0') {
-          showLen = col - (modEnd * COL_SIZE);  // padding characters
-          state = 2;
-          break;
-        }
-
-        // retrieve the next character form the font file
-        showLen = mx.getChar(*pMsg++, sizeof(cBuf) / sizeof(cBuf[0]), cBuf);
-        curLen = 0;
-        state++;
-        // !! deliberately fall through to next state to start displaying
-
-      case 1:  // display the next part of the character
-        mx.setColumn(col--, cBuf[curLen++]);
-
-        // done with font character, now display the space between chars
-        if (curLen == showLen) {
-          showLen = CHAR_SPACING;
-          state = 2;
-        }
-        break;
-
-      case 2:  // initialize state for displaying empty columns
-        curLen = 0;
-        state++;
-        // fall through
-
-      case 3:  // display inter-character spacing or end of message padding (blank columns)
-        mx.setColumn(col--, 0);
-        curLen++;
-        if (curLen == showLen)
-          state = 0;
-        break;
-
-      default:
-        col = -1;  // this definitely ends the do loop
-    }
-  } while (col >= (modStart * COL_SIZE));
-
-  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
-}
-
-void scrollText(const char* p) {
-  uint8_t charWidth;
-  uint8_t cBuf[8];  // this should be ok for all built-in fonts
-
- // PRINTS("\nScrolling text");
-  mx.clear();
-
-  while (*p != '\0') {
-    charWidth = mx.getChar(*p++, sizeof(cBuf) / sizeof(cBuf[0]), cBuf);
-
-    for (uint8_t i = 0; i <= charWidth; i++)  // allow space between characters
-    {
-      mx.transform(MD_MAX72XX::TSL);
-      mx.setColumn(0, (i < charWidth) ? cBuf[i] : 0);
-      delay(100);
-    }
-  }
-}
-*/
-
-/*
-// TODO combine with the poti
-
-void intensity()
-// Demonstrates the control of display intensity (brightness) across
-// the full range.
-{
-  uint8_t row;
-
-  PRINTS("\nVary intensity ");
-
-  mx.clear();
-
-  // Grow and get brighter
-  row = 0;
-  for (int8_t i=0; i<=MAX_INTENSITY; i++)
-  {
-    mx.control(MD_MAX72XX::INTENSITY, i);
-    if (i%2 == 0)
-      mx.setRow(row++, 0xff);
-    delay(DELAYTIME*3);
-  }
-
-  mx.control(MD_MAX72XX::INTENSITY, 8);
-}*/
-
 void readFromHA() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -184,7 +82,6 @@ void readFromHA() {
     Serial.print("Error on HTTP request: ");
     Serial.println(httpResponseCode);
   }
-
   http.end();
 }
 
@@ -210,11 +107,6 @@ void setup() {
 #elif defined(ESP32)
   client.setCACert(spotify_server_cert);
 #endif
-  // ... or don't!
-  //client.setInsecure();
-
-  // If you want to enable some extra debugging
-  // uncomment the "#define SPOTIFY_DEBUG" in SpotifyArduino.h
 
   Serial.println("Refreshing Access Tokens");
   if (!spotify.refreshAccessToken()) {
@@ -248,44 +140,39 @@ String httpGETRequest(const char* serverName) {
 
   return payload;
 }
-/*
+
 void showTemperatureOutside() {
   if (WiFi.status() == WL_CONNECTED) {
     String serverPath = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode + "&APPID=" + openWeatherMapApiKey + "&units=metric";
 
     jsonBuffer = httpGETRequest(serverPath.c_str());
     Serial.println(jsonBuffer);
-    JSONVar myObject = JSON.parse(jsonBuffer);
+    DynamicJsonDocument myObject(1024 * 2);  // Adjust the size according to your JSON string
 
-    // JSON.typeof(jsonVar) can be used to get the type of the var
-    if (JSON.typeof(myObject) == "undefined") {
-      Serial.println("Parsing input failed!");
+    DeserializationError error = deserializeJson(myObject, jsonBuffer);
+    if (error) {
+      Serial.print("Deserialization failed: ");
+      Serial.println(error.c_str());
       return;
     }
 
-    Serial.print("JSON object = ");
-    Serial.println(myObject);
     Serial.print("Temperature: ");
-    Serial.println(myObject["main"]["temp"]);
-    /*Serial.print("Pressure: ");
-    Serial.println(myObject["main"]["pressure"]);
-    Serial.print("Humidity: ");
-    Serial.println(myObject["main"]["humidity"]);
-    Serial.print("Wind Speed: ");
-    Serial.println(myObject["wind"]["speed"]);//
+    float temp = myObject["main"]["temp"];
+    Serial.println(temp);
+    char message[8] = "0.00";
+    sprintf(message, "%.1fC", temp);
+    Serial.println(message);
+    mx.displayText(message, PA_CENTER, 25, 500, PA_PRINT, PA_PRINT);
+    mx.displayAnimate();
   } else {
     Serial.println("WiFi Disconnected");
   }
-}*/
+}
 
 // todo  P.displayShutdown(true); if it is switchd off?
 
 
 void showTime() {
-  // todo don't load it from the internet everytime
-  //         mx.setTextAlignment(PA_CENTER);
-  //       mx.displayClear();
-  //       mx.displayReset();
   timeClient.update();
   int hour = timeClient.getHours();
   int minute = timeClient.getMinutes();
@@ -298,37 +185,9 @@ void showTime() {
 
 void printCurrentlyPlayingSpotify(CurrentlyPlaying currentlyPlaying) {
   if (currentlyPlaying.isPlaying) {
-    /* Serial.print("Track: ");
-    Serial.println(currentlyPlaying.trackName);
-    for (int i = 0; i < currentlyPlaying.numArtists; i++) {
-      Serial.print("Name: ");
-      Serial.println(currentlyPlaying.artists[i].artistName);
-    }
-    Serial.print("Album: ");
-    Serial.println(currentlyPlaying.albumName);
-
-
-    long progress = currentlyPlaying.progressMs;  // duration passed in the song
-    long duration = currentlyPlaying.durationMs;  // Length of Song
-    Serial.print("Elapsed time of song (ms): ");
-    Serial.print(progress);
-    Serial.print(" of ");
-    Serial.println(duration);
-    Serial.println();*/
     char message[256] = "";
     sprintf(message, "%s - %s", currentlyPlaying.trackName, currentlyPlaying.artists[0].artistName);
-    mx.displayAnimate();
-    mx.displayScroll(message, PA_LEFT, PA_PRINT, 25);
-    Serial.println("Message to print");
-    Serial.println(message);
-    //   mx.displayText(message, PA_LEFT, 25, 500, PA_SCROLL_LEFT, PA_PRINT);
 
-    // mx.displayReset();
-    //   mx.displayText("mge", PA_LEFT, 25, 2000, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-    //       mx.displayText("Your Scrolling Text Here", PA_CENTER, PA_SCROLL_LEFT, 1000);
-    //mx.run();
-    //mx.displayAnimate();
-    //scrollText(message);
     displayScrollText(message);
   } else {
     Serial.println("Spotify is currently not playing");
@@ -346,18 +205,19 @@ void showSpotifyCurrentlyPlaying() {
     Serial.println(status);
   }
 }
-void displayScrollText(char* message){
+void displayScrollText(char* message) {
   mx.displayClear();
-  mx.displayScroll(message, PA_RIGHT, PA_SCROLL_LEFT, 25);
+  mx.displayScroll(message, PA_RIGHT, PA_SCROLL_LEFT, 40);
   while (!mx.displayAnimate()) { ; }
 }
 
 void loop() {
-   for(int i=0;i<20;i++){
-  mx.setIntensity(50);
-  showTime();
-  delay(1000);
+  /*  for (int i = 0; i < 1; i++) {
+    mx.setIntensity(10);
+    showTime();
+    delay(1000);
   }
-  showSpotifyCurrentlyPlaying();
-  //showTemperatureOutside();
+  showSpotifyCurrentlyPlaying();*/
+  showTemperatureOutside();
+  delay(100000);
 }
